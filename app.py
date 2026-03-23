c1762d5324d1b0539df8a9aa38b8cf5fe1b46e1a
Vaidik Srivastava <vaidiksrivastava786786@gmail.com>
2026-03-23 19:11:40 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -57,4 +58,5 @@ def verify():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
