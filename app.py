e52ddcc10775c7bc77660d75efb599440332dd53
Vaidik Srivastava <vaidiksrivastava786786@gmail.com>
2026-03-24 09:41:13 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def verify():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
